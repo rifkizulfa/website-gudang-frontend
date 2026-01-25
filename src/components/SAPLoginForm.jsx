d6ba0458b095209dcd5dd9e348360e8c7fff29e5
rifki <rifkigalvatron202@gmail.com>
2026-01-25 23:37:21 +0700

    add axios config

--- src/components/SAPLoginForm.jsx
+++ src/components/SAPLoginForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import apiClient from "../api/axiosConfig";
+import apiClient from "../axiosConfig";
 import "../styles/sap-style.css";
 import { useNavigate } from "react-router-dom";
 
