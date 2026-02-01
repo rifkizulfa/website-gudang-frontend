8605eda753cd02dd2196977f433ab4b9b5ed7a14
rifki <rifkigalvatron202@gmail.com>
2026-02-01 13:40:59 +0700

    h ke 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-import './index.css';
 import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SAPSystemSelector from './components/SAPSystemSelector';
